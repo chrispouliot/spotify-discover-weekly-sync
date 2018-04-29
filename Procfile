78053be659408d510bc888c8f1261201bfff1e71
Christophe Pouliot <cristophepoug@gmail.com>
2018-04-29 11:59:37 -0700

    Remove run command in favour of Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -b --workers=2 app:app
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
+from musicsync import SpotifyOAuth
 
 # Server static through nginx in future
 app = Flask(__name__, template_folder="client", static_folder="client")
@@ -6,4 +7,9 @@ app = Flask(__name__, template_folder="client", static_folder="client")
 
 @app.route('/')
 def index():
+    return redirect(SpotifyOAuth.get_oauth_url())
+
+
+@app.route('/callback')
+def callback():
     return render_template('index.html')
